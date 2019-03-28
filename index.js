6f400e5ce4283f98e14065f7c7c3d6403c4c606b
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 14:15:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function  updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
     return Object.assign(object, {[key]: value});
 }
+function deleteFromObjectByKey(object, key){
+  //const doSomething = (obj, prop) => {
+  let res = Object.assign({}, object)
+  delete res[key]
+  return res
+}
