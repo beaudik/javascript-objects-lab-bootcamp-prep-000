47ebefe74155d174386fbc957dab83be8773bbde
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 13:49:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
-function  updateObjectWithKeyAndValue(obj, key, value){
-  return object.assign({}, obj, {[key]: value});
+function  updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value});
 }
